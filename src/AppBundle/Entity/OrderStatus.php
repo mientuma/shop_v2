54b9fe20091430fe100ae7fad4f4b8fcdd2c2a6d
Mateusz <mateusz.mientus95@gmail.com>
2017-03-14 11:56:56 +0100

    Dodano statusy do db

---
+++ src/AppBundle/Entity/OrderStatus.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * OrderStatus
+ *
+ * @ORM\Table(name="order_status")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\OrderStatusRepository")
+ */
+class OrderStatus
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="status", type="string", length=255, unique=true)
+     */
+    private $status;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set status
+     *
+     * @param string $status
+     *
+     * @return OrderStatus
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+}
